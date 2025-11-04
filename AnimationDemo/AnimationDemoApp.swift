4889aab13eee8fb765b52ac7b94f956b2bda3ac1
Didier Delhaisse <did35@yahoo.com>
2025-11-04 17:05:04 +0100

    Initial Commit

---
+++ AnimationDemo/AnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDemoApp.swift
+//  AnimationDemo
+//
+//  Created by Didier Delhaisse on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
